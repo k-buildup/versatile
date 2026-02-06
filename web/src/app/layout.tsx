4cb18276d55375508050cdf7d3a4bd61d3b16133
ICe1BotMaker <ice1github@gmail.com>
2026-02-06 16:43:07 +0900

    (under-development) src 디렉터리로 변경

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en" className="bg-gray-950">
+        <html lang="en">
             <body>{children}</body>
         </html>
     );
---
+++ web/src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+    return <div className="">엄준식</div>;
+}
