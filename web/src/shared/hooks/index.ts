3ebcdf13d234e7bd62693e5d7b0466b5356ac694
ICe1BotMaker <ice1github@gmail.com>
2026-02-09 19:40:42 +0900

    [remove] next.js 마이그레이션 취소

--- web/src/shared/hooks/index.ts
+++
@@ -1,3 +0,0 @@
-export { useAuth } from "./use-auth";
-export { useChat } from "./use-chat";
-export { useSession } from "./use-session";
--- web/src/shared/types/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./message";
-export * from "./session";
-export * from "./user";
